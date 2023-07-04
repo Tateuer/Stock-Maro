a04309ba98a94d41dea27e927d6ae6c8df39118b
ayrton.wuthrich <ayrton.wuthrich@hotmail.com>
2023-07-04 10:34:40 -0300

    Agregando consumibles

--- client/src/Components/ConsumiblesContainer.jsx
+++ client/src/Components/ConsumiblesContainer.jsx
@@ -1,8 +1,31 @@
+import { Box, Grid } from "@chakra-ui/react";
+import { getConsumibles } from "../asyncMock";
+import { useEffect, useState } from "react";
+
 const ConsumiblesContainer = () => {
+    const [consumibles, setConsumibles] = useState([]);
+
+    useEffect(() => {
+        const fetchConsumibles = async () => {
+            try {
+                const consumiblesData = await getConsumibles();
+                setConsumibles(consumiblesData);
+            } catch (error) {
+                console.error("Error al obtener los consumibles", error);
+            }
+        };
+        fetchConsumibles();
+    }, []);
+
     return (
-        <div>
-            <h2>Esto son los consumibles:</h2>
-        </div>
+        <Box>
+            <h2>Consumibles</h2>
+            <Box>
+                {consumibles.map((consumible) => (
+                    <Grid key={consumible.id}> {consumible.detalle} </Grid>
+                ))}
+            </Box>
+        </Box>
     );
 };
 
--- client/src/asyncMock.jsx
+++ client/src/asyncMock.jsx
@@ -59,6 +59,28 @@ export const getClientes = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(clientes);
-        }, 500);
+        }, 250);
+    });
+};
+
+const consumibles = [
+    { id: "1", detalle: "Tornillos alem M4, 5, 6, 8, 10, 12, 16" },
+    { id: "2", detalle: "Tornillos cabeza frasada M4, 5, 6, 8, 10, 12, 16" },
+    { id: "3", detalle: "Electrodos" },
+    { id: "4", detalle: "Grasa" },
+    { id: "5", detalle: 'Lubricante multiuso "Wd40"' },
+    { id: "6", detalle: "Cuños" },
+    { id: "7", detalle: "Espinas diámetro 4, 6, 8, 10, 12" },
+    { id: "8", detalle: "Tinta de trazar" },
+    { id: "9", detalle: "Tornillo guias" },
+    { id: "10", detalle: "Resortes" },
+    { id: "11", detalle: "Picos para soldadura tig" },
+];
+
+export const getConsumibles = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(consumibles);
+        }, 250);
     });
 };
