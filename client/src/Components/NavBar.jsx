d0ad8c97c5265c800f88c7ee9f7f85144ce8b514
Tateuer <tateuer@gmail.com>
2023-07-06 12:47:01 -0300

    git  push

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -62,51 +62,5 @@ const NavBar = () => {
             ) : null}
         </Box>
     );
-  return (
-    <Box>
-      <GridItem>
-        <Link to="/">
-          <Image
-            marginTop="-5rem"
-            paddingBottom="2rem"
-            gridArea={"logo"}
-            src={LogoMaro}
-            alt="Maro S.A"
-          />
-        </Link>
-      </GridItem>
-      {location.pathname !== "/" && (
-        <Button fontSize="20px" onClick={handleGoBack}>
-          Atrás
-        </Button>
-      )}
-      {location.pathname !== "/cargarpieza" &&
-      location.pathname !== "/piezafaltante" &&
-      location.pathname !== "/cilindros" &&
-      location.pathname !== "/consumibles" &&
-      location.pathname !== "/piezaseguridad" &&
-      location.pathname !== "/qr" &&
-      location.pathname !== "/stock" &&
-      location.pathname !== "/clientes" ? (
-        <Menu>
-          <Grid
-            templateRows={"repeat(3, 1fr)"}
-            templateColumns={"repeat(2, 1fr)"}
-            fontSize="1.4rem"
-          >
-            <Link to="/cargarpieza">Cargar pieza</Link>
-            <Link to="/piezafaltante">Piezas faltantes</Link>
-            <Link to="/cilindros">Cilindros</Link>
-            <Link to="/consumibles">Consumibles</Link>
-            <Link to="/piezaseguridad">Piezas de seguridad</Link>
-            <Link to="/clientes">Clientes</Link>
-            <Link to="/stock">Stock</Link>
-            <Link to="/qr">QRs</Link>
-          </Grid>
-        </Menu>
-      ) : null}
-    </Box>
-  );
 };
 
-export default NavBar;
